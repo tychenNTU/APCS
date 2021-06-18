42c2b102bf89d778898640bd42bd3777f80b1401
tychenNTU <68041127+tychenNTU@users.noreply.github.com>
2021-06-18 20:51:46 +0800

    Update ZeroJudge [BFS] e699 11624 - Fire.cpp

--- 圖論/ZeroJudge [BFS] e699 11624 - Fire.cpp
+++ 圖論/ZeroJudge [BFS] e699 11624 - Fire.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
-
+#define NMAX 101
 using namespace std;
 
 int main(){
-	int t, r, c, fire_time[101][101], start_y, start_x;
+	int t, r, c, fire_time[NMAX][NMAX], start_y, start_x, ans;
 	int dir_x[4] = {1, 0, -1, 0}, dir_y[4] = {0, 1, 0, -1};
-	bool wall[101][101];
+	bool wall[NMAX][NMAX], vis[NMAX][NMAX];
+	queue<pair<pair<int, int>, int>> q;
 	string x;
+	cin.tie();
+	ios::sync_with_stdio(false);
 	cin >> t;
 	for(int qq = 0;qq < t;qq ++){
-		queue<pair<pair<int, int>, int>> q;
+		ans = -1;
 		cin >> r >> c;
 		for(int i=0;i<r;i++){
 			for(int j=0;j<c;j++){
 				fire_time[i][j] = -1;
+				vis[i][j] = false;
 			}
 		}
 		for(int i=0;i<r;i++){
@@ -49,6 +53,26 @@ int main(){
 			}
 			cout << "\n";
 		}*/
+		q.push({{start_y, start_x}, 0});
+		vis[start_y][start_x] = true;
+		while(!q.empty()){
+			pair<pair<int, int>, int> now = q.front();
+			q.pop();
+			if(ans != -1) continue;
+			int now_y = now.first.first, now_x = now.first.second, now_step = now.second;
+			for(int i=0;i<4;i++){
+				int next_x = now_x + dir_x[i], next_y = now_y + dir_y[i];
+				if(next_x < 0 || next_x >= c || next_y < 0 || next_y >= r){
+					ans = now_step + 1;
+					break;
+				}
+				if(wall[next_y][next_x] || fire_time[next_y][next_x] <= now_step + 1 || vis[next_y][next_x]) continue;
+				vis[next_y][next_x] = true;
+				q.push({{next_y, next_x}, now_step + 1});
+			}
+		}
+		if(ans == -1) cout << "IMPOSSIBLE\n";
+		else cout << ans << "\n";
 	}
     return 0;
 }
