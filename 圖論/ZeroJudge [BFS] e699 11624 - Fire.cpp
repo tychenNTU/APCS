42c7b528322d4682aa862a089a072b7fdf8ff3dd
tychenNTU <68041127+tychenNTU@users.noreply.github.com>
2021-06-18 20:08:49 +0800

    1

---
+++ 圖論/ZeroJudge [BFS] e699 11624 - Fire.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t, r, c, fire_time[1010][1010], start_y, start_x;
+	int dir_x[4] = {1, 0, -1, 0}, dir_y[4] = {0, 1, 0, -1};
+	bool wall[1010][1010];
+	string x;
+	cin >> t;
+	for(int q = 0;q < t;q ++){
+		queue<pair<pair<int, int>, int>> q;
+		cin >> r >> c;
+		for(int i=0;i<r;i++){
+			cin >> x;
+			for(int j=0;j<c;j++){
+				if(x[j] == '#') wall[i][j] = true;
+				else wall[i][j] = false;
+				if(x[j] == 'F'){
+					q.push({{i, j}, 0});
+					fire_time[i][j] = 0;
+				}
+				if(x[j] == 'J'){
+					start_y = i;
+					start_x = j;
+				}
+			}
+		}
+		while(!q.empty()){
+			pair<pair<int, int>, int> now = q.front();
+			q.pop();
+			int now_y = now.first.first, now_x = now.first.second, now_step = now.second;
+			for(int i=0;i<4;i++){
+				int next_x = now_x + dir_x[i], next_y = now_y + dir_y[i];
+				if(next_x < 0 || next_x >= c || next_y < 0 || next_y >= r) continue;
+				if(wall[next_y][next_x] || fire_time[next_y][next_x] >= 0) continue;
+				fire_time[next_y][next_x] = now_step + 1;
+				q.push({{next_y, next_x}, now_step + 1});
+			}
+		}
+	}
+    return 0;
+}
